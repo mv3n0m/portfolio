a8f35a1bcb5f119e5869c3664f07c8dd7224d1e9
Manish Roy <manish.11612939@lpu.in>
2023-04-21 13:48:11 +0530

    created Hero component

---
+++ src/app/Hero.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef } from 'react'
+import Github from 'assets/icons/github.svg'
+import Linkedin from 'assets/icons/linkedin.svg'
+import Typed from "typed.js";
+
+
+function Hero() {
+    const el = useRef(null);
+
+    const typingStrings = ["Learner", "Generalist", "Fullstack Engineer", "Web3 Developer", "Freelancer", "Problem Solver", "Critical Thinker", "Software Engineer"]
+
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: typingStrings,
+            startDelay: 300,
+            typeSpeed: 50,
+            backSpeed: 20,
+            backDelay: 500
+        });
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+
+
+    return (
+        <div className='hero'>
+            <h1>¡Hola!</h1>
+            <h2 className='title'>I'm <span className='name'> Manish Roy</span></h2>
+            <h2 className='typed'>. . . a <span ref={ el }></span></h2>
+            <div className='btns'>
+                <button className='hire-me btn'>Hire Me!</button>
+                <button className='view-cv btn'>View CV</button>
+            </div>
+            <div className='actions'>
+                <img src={ Linkedin } width={ 30 } />
+                <img src={ Github } width={ 30 } className="gt" />
+            </div>
+        </div>
+    )
+}
+
+export default Hero
