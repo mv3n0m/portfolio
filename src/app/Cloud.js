c5df462adc35d1f357001fc9921f57979bfa0971
Manish Roy <manish.11612939@lpu.in>
2023-04-21 17:27:01 +0530

    implemented wordcloud

---
+++ src/app/Cloud.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef } from 'react'
+import WordCloud from 'wordcloud'
+
+
+function Cloud({ darkMode }) {
+    const el = useRef(null)
+    const words = [
+        ["Python", 11],
+        ["Javascript", 12],
+        ["React.js", 11],
+        ["Flask", 10],
+        ["Solidity", 8],
+        ["Rust", 4],
+        ["Ethereum", 9],
+        ["MongoDB", 10],
+        ["SQL", 6],
+        ["Blockchain", 7],
+        ["Web3", 7],
+        ["Solana", 4],
+        ["Django", 6],
+        ["Bootstrap", 8],
+        ["HTML", 8],
+        ["CSS", 7],
+        ["AWS", 9]
+    ]
+    const colors = [
+        "#F2BD5A",
+        "#005554",
+        "#215641",
+        "#EEC16D",
+        "#3E877F",
+        "#AFC07C",
+        "#53B59F",
+        "#5FB962",
+        darkMode ? "#E3E1D5" : "#10353B"
+    ]
+
+    useEffect(() => {
+        WordCloud(el.current, {
+            list: words,
+            backgroundColor: "transparent",
+            weightFactor: 3,
+            color: (_, w) => colors[12 - w],
+            rotateRatio: 0.3,
+            rotationSteps: 2,
+            fontWeight: "bold",
+            gridSize: 30
+
+        })
+    }, [ darkMode ])
+
+
+    return (
+        <div className='cloud'>
+            <canvas ref={ el } width={ 1000 } />
+        </div>
+    )
+}
+
+export default Cloud
