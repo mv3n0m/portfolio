62afd9e57b5ccb5b398b20678e5d23486b0ebc38
Manish Roy <manish.11612939@lpu.in>
2023-04-21 13:48:56 +0530

    integrated theme toggle and Hero components to Page

--- src/app/Page.js
+++ src/app/Page.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Sun from 'assets/icons/sun.svg'
+import Moon from 'assets/icons/moon.svg'
+import Header from 'assets/images/header.png'
+import Hero from './Hero'
+
 
 function Page() {
-  return (
-    <div>Page</div>
-  )
+    const [ toggled, setToggled ] = useState(localStorage.getItem("darkMode") || false)
+    const theme = toggled ? "dark" : "light"
+    const icon = toggled ? Sun : Moon
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme)
+        toggled ? localStorage.setItem("darkMode", true) : localStorage.removeItem("darkMode")
+    }, [ toggled ])
+
+    const handleToggle = () => {
+        setToggled(!toggled)
+    }
+
+    return (
+        <div className='page'>
+            <img src={ icon } onClick={ handleToggle } className="pointer toggle" width={ 25 } />
+            <div className='header'>
+                <Hero />
+                <img src={ Header } width={ 400 } />
+            </div>
+        </div>
+    )
 }
 
 export default Page
